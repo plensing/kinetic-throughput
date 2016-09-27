1fc4a78ce230e6009d6832682add7497bb23d720
Paul Lensing <paul.h.lensing@seagate.com>
2016-09-27 16:41:13 -0700

    Improved random sequence operation so generated keys can be used for write and get operation sequences. Added start_key to allow choosing differetn spindle positions for get operation.

--- throughput.cc
+++ throughput.cc
@@ -3,6 +3,7 @@
 #include <thread>
 #include <sstream>
 #include <iomanip>
+#include <algorithm>
 #include "kinetic/kinetic.h"
 
 using kinetic::KineticConnectionFactory;
@@ -23,6 +24,7 @@ enum class conselect{
 struct configuration{
   int num_threads;
   int num_keys;
+  int starting_key;
   int value_size;
   int report_keys;
   bool flush_on_report;
@@ -52,6 +54,8 @@ void parse(int argc, char** argv, configuration &config)
       config.num_threads = std::stoi(argv[i+1]);
     if(strcmp("-keys", argv[i]) == 0)
       config.num_keys = std::stoi(argv[i+1]);
+    if(strcmp("-starting_key", argv[i]) == 0)
+      config.starting_key = std::stoi(argv[i+1]);
     if(strcmp("-size", argv[i]) == 0)
       config.value_size = std::stoi(argv[i+1]);
     if(strcmp("-host", argv[i]) == 0)
@@ -90,6 +94,8 @@ void parse(int argc, char** argv, configuration &config)
 
   if(!config.report_keys)
     config.report_keys = config.num_keys;
+  if(!config.random_sequence_size)
+    config.random_sequence_size = config.num_keys;
 
   printf("---------------------------------------------------------- \n");
   printf("                cpp client throughput test               \n");
@@ -104,14 +110,15 @@ void parse(int argc, char** argv, configuration &config)
             "  Identity used for connection(s): -id %d \n"
             "  Number of threads: -threads %d \n"
             "  Number of keys: -keys %d \n"
-            "  Size of keys in KB: -size %d \n"
+            "  Starting key number: -starting_key %d\n"
+            "  Size of key value in KB: -size %d \n"
             "  Report average performance ever n keys: -report %d \n"
             "  Flush connection before reporting {enabled,disabled}: -flush %s \n"
-            "  Put / Delete persistence {write_back,write_through}: %s   \n"
+            "  Put / Delete persistence {write_back,write_through}: -persist %s \n"
             "  Selection strategy for connections {hash,fixed}: -select %s \n"
-            "  Randomize key every n sequential operations, a value of 0 disables randomization: -ran_seq %d \n",
+            "  Randomize key every n sequential operations: -ran_seq %d \n",
           config.security_key.c_str(), config.security_id,
-          config.num_threads, config.num_keys, config.value_size, config.report_keys,
+          config.num_threads, config.num_keys, config.starting_key, config.value_size, config.report_keys,
           config.flush_on_report ? "enabled" : "disabled",
           config.persist ==  kinetic::PersistMode::WRITE_BACK ? "write_back" : "write_through",
           config.select == conselect::HASH ? "hash" : "fixed",
@@ -141,67 +148,81 @@ void connect(const configuration &config, vector<shared_ptr<kinetic::BlockingKin
   }
 }
 
-void test(int tid, int start_key, OperationType type, const configuration& config, vector<shared_ptr<kinetic::BlockingKineticConnectionInterface>> &cons)
+void test(int thread_id,
+          int operationFrequency,
+          OperationType operationType,
+          std::list<uint32_t>::iterator& operationKeys,
+          const configuration& config,
+          vector<shared_ptr<kinetic::BlockingKineticConnectionInterface>>& cons)
 {
+  string value(config.value_size, 'X');
+  int connectionID = thread_id % cons.size();
 
-  int key_base = 0;
-  string value;
-  value.resize(config.value_size, 'X');
-
-  int connectionID = tid % cons.size();
-  int keys_per_thread = config.report_keys / config.num_threads;
-
-  for(int i=0; i<keys_per_thread; i++){
-    int block_number = start_key+i;
-
+  for (int i = 0; i < operationFrequency; i++) {
     std::ostringstream ss;
-    if(config.random_sequence_size && (block_number % config.random_sequence_size == 0)) {
-      key_base = rand();
-    }
-    ss << std::setw(10) << std::setfill('0') << key_base;
-    ss << std::setw(10) << std::setfill('0') << block_number;
+    ss << std::setw(20) << std::setfill('0') << *operationKeys++;
     auto key = ss.str();
 
-    if(block_number < 10)
-      printf("debug key %d is %s\n",block_number,key.c_str());
+    //printf("thread %d -> %s\n",thread_id,key.c_str());
 
-    if(config.select == conselect::HASH)
+    if(config.select == conselect::HASH) {
       connectionID = std::hash<string>()(key) % cons.size();
+    }
 
     kinetic::KineticStatus status = kinetic::KineticStatus(kinetic::StatusCode::REMOTE_OTHER_ERROR, "");
-
-    switch(type){
-      case OperationType::PUT:{
-        KineticRecord record(value, std::to_string((long long int)i), "", com::seagate::kinetic::client::proto::Command_Algorithm_SHA1);
+    switch (operationType) {
+      case OperationType::PUT: {
+        KineticRecord record(value, std::to_string((long long int) i), "",
+                             com::seagate::kinetic::client::proto::Command_Algorithm_SHA1);
         status = cons[connectionID]->Put(key, "", WriteMode::IGNORE_VERSION, record, config.persist);
       }
         break;
-      case OperationType::GET:{
+      case OperationType::GET: {
         std::unique_ptr<KineticRecord> record;
         status = cons[connectionID]->Get(key, record);
       }
         break;
-      case OperationType::DEL:{
+      case OperationType::DEL: {
         status = cons[connectionID]->Delete(key, "", WriteMode::IGNORE_VERSION, config.persist);
       }
         break;
-      case OperationType::LOG:{
+      case OperationType::LOG: {
         std::unique_ptr<kinetic::DriveLog> log;
         status = cons[connectionID]->GetLog({kinetic::Command_GetLog_Type::Command_GetLog_Type_UTILIZATIONS}, log);
       }
         break;
     }
-    if(!status.ok())
-      printf("ERROR DURING %s OPERATION: %s \n",to_str(type).c_str(), status.message().c_str());
+    if (!status.ok()) {
+      printf("ERROR DURING %s OPERATION: %s \n", to_str(operationType).c_str(), status.message().c_str());
+    }
   }
 
-  if(config.flush_on_report)
+  if (config.flush_on_report) {
     cons[connectionID]->Flush();
+  }
+}
+
+std::list<uint32_t> constructAccessList(const struct configuration& config) {
+
+  std::vector<std::list<uint32_t>> sequence_vector(config.num_keys / config.random_sequence_size);
+  for (size_t sequence = 0; sequence < sequence_vector.size(); sequence++) {
+    for (size_t number = sequence * config.random_sequence_size;
+         number < (sequence + 1) * config.random_sequence_size; number++) {
+      sequence_vector[sequence].push_back(number);
+    }
+  }
+  std::random_shuffle(sequence_vector.begin(), sequence_vector.end());
+
+  std::list<uint32_t> access_list;
+  for (size_t sequence = 0; sequence < sequence_vector.size(); sequence++) {
+    access_list.splice(access_list.begin(), sequence_vector[sequence]);
+  }
+  return access_list;
 }
 
 int main(int argc, char** argv)
 {
-  struct configuration config = {1,100,0,0,false,kinetic::PersistMode::WRITE_BACK,conselect::FIXED,{},{},1,"asdfasdf",0};
+  struct configuration config = {1,100,0,0,0,false,kinetic::PersistMode::WRITE_BACK,conselect::FIXED,{},{},1,"asdfasdf",0};
   parse(argc, argv, config);
 
   vector<shared_ptr<kinetic::BlockingKineticConnectionInterface>> cons;
@@ -211,33 +232,43 @@ int main(int argc, char** argv)
     exit(0);
   }
 
-  for(size_t o=0; o<config.ops.size(); o++){
+  auto access_list = constructAccessList(config);
+
+  for (size_t o = 0; o < config.ops.size(); o++) {
 
     auto op = config.ops[o];
-
     auto num_runs = config.num_keys / config.report_keys;
-    for(int r = 0; r < num_runs; r++){
+    auto ops_per_thread = config.num_keys / (config.num_threads * num_runs);
 
+    std::vector<std::list<uint32_t>::iterator> thread_iterators;
+    std::list<uint32_t>::iterator cursor = access_list.begin();
+    for(int t = 0; t < config.num_threads; t++) {
+      thread_iterators.push_back(cursor);
+      std::advance(cursor, config.num_keys/config.num_threads);
+    }
+
+    for (int r = 0; r < num_runs; r++) {
       vector<std::thread> threads;
       auto run_start = system_clock::now();
 
-      for(int i=0; i<config.num_threads; i++){
-        int start_key = i*(config.report_keys/config.num_threads) + r*config.report_keys;
-        threads.push_back(std::thread(std::bind(test, i, start_key, op, std::cref(config), std::ref(cons))));
+      for (int i = 0; i < config.num_threads; i++) {
+        threads.push_back(std::thread(std::bind(
+            test, i, ops_per_thread, op, std::ref(thread_iterators[i]), std::cref(config), std::ref(cons))));
       }
-      for(size_t t = 0; t<threads.size(); t++)
+      for (size_t t = 0; t < threads.size(); t++) {
         threads[t].join();
+      }
 
-      auto run_end  = system_clock::now();
-      int  duration = (int) duration_cast<milliseconds>(run_end-run_start).count();
+      auto run_end = system_clock::now();
+      int duration = (int) duration_cast<milliseconds>(run_end - run_start).count();
 
-      printf( "\n%s of %d keys done in %d milliseconds "
-                  "\n\t -->  %f MB/second"
-                  "\n\t -->  %f keys/second"
-                  "\n",
-              to_str(op).c_str(),config.report_keys, duration,
-              (config.report_keys*((float)config.value_size / (1024*1024))) / ( duration / 1000.0),
-              config.report_keys / (duration / 1000.0)
+      printf("\n%s of %d keys done in %d milliseconds "
+                 "\n\t -->  %f MB/second"
+                 "\n\t -->  %f keys/second"
+                 "\n",
+             to_str(op).c_str(), config.report_keys, duration,
+             (config.report_keys * ((float) config.value_size / (1024 * 1024))) / (duration / 1000.0),
+             config.report_keys / (duration / 1000.0)
       );
       fflush(stdout);
     }
