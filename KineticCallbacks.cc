7f7c7683897e25cc9ca4942d567a4c0fc32a6f0b
Paul Lensing <paul.h.lensing@seagate.com>
2016-11-04 10:00:08 -0700

    Now with async requests with defined queue depth. Re-using the kineticio callbacks for that.

---
+++ KineticCallbacks.cc
@@ -0,0 +1,229 @@
+/************************************************************************
+ * KineticIo - a file io interface library to kinetic devices.          *
+ *                                                                      *
+ * This Source Code Form is subject to the terms of the Mozilla         *
+ * Public License, v. 2.0. If a copy of the MPL was not                 *
+ * distributed with this file, You can obtain one at                    *
+ * https://mozilla.org/MP:/2.0/.                                        *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but is provided AS-IS, WITHOUT ANY WARRANTY; including without       *
+ * the implied warranty of MERCHANTABILITY, NON-INFRINGEMENT or         *
+ * FITNESS FOR A PARTICULAR PURPOSE. See the Mozilla Public             *
+ * License for more details.                                            *
+ ************************************************************************/
+
+#include "KineticCallbacks.hh"
+
+using namespace kio;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+CallbackSynchronization::CallbackSynchronization() : outstanding(0), cv(), mutex()
+{ }
+
+CallbackSynchronization::~CallbackSynchronization()
+{ }
+
+void CallbackSynchronization::wait_until(std::chrono::system_clock::time_point timeout_time)
+{
+  std::unique_lock<std::mutex> lck(mutex);
+  while (outstanding && std::chrono::system_clock::now() < timeout_time) {
+    cv.wait_until(lck, timeout_time);
+  }
+}
+
+
+void CallbackSynchronization::run_until(std::unique_ptr <kinetic::ThreadsafeNonblockingKineticConnection>& con,
+                                        int max_outstanding)
+{
+  fd_set a; int b;
+  while(outstanding >= max_outstanding){
+    con->Run(&a, &a, &b);
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+KineticCallback::KineticCallback(std::shared_ptr<CallbackSynchronization> s) :
+    status(kinetic::KineticStatus(kinetic::StatusCode::CLIENT_INTERNAL_ERROR, "no result")),
+    sync(std::move(s)),
+    done(false)
+{
+  sync->outstanding++;
+}
+
+KineticCallback::~KineticCallback()
+{ }
+
+void KineticCallback::OnResult(kinetic::KineticStatus result)
+{
+  std::unique_lock<std::mutex> lock(sync->mutex);
+  if (done) {
+    return;
+  }
+  if (!result.ok()){
+    printf("Operation Failed.\n");
+  }
+
+  status = result;
+  done = true;
+  sync->outstanding--;
+  if (!sync->outstanding) {
+    sync->cv.notify_one();
+  }
+}
+
+kinetic::KineticStatus& KineticCallback::getResult()
+{
+  std::lock_guard<std::mutex> lock(sync->mutex);
+  return status;
+}
+
+bool KineticCallback::finished()
+{
+  std::lock_guard<std::mutex> lock(sync->mutex);
+  return done;
+}
+
+void KineticCallback::reset()
+{
+  std::lock_guard<std::mutex> lock(sync->mutex);
+  done = false;
+  status = kinetic::KineticStatus(kinetic::StatusCode::CLIENT_INTERNAL_ERROR, "no result");
+  sync->outstanding++;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using namespace kinetic;
+
+GetCallback::GetCallback(std::shared_ptr<CallbackSynchronization> s) : KineticCallback(std::move(s))
+{ }
+
+GetCallback::~GetCallback()
+{ }
+
+void GetCallback::Success(const std::string& key, std::unique_ptr<kinetic::KineticRecord> r)
+{
+  record = std::move(r);
+  OnResult(KineticStatus(StatusCode::OK, ""));
+}
+
+void GetCallback::Failure(KineticStatus error)
+{
+  OnResult(error);
+};
+
+std::shared_ptr<KineticRecord>& GetCallback::getRecord()
+{
+  return record;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+GetVersionCallback::GetVersionCallback(std::shared_ptr<CallbackSynchronization> s) : KineticCallback(std::move(s))
+{ }
+
+GetVersionCallback::~GetVersionCallback()
+{ }
+
+void GetVersionCallback::Success(const std::string& v)
+{
+  version = v;
+  OnResult(KineticStatus(StatusCode::OK, ""));
+}
+
+void GetVersionCallback::Failure(KineticStatus error)
+{
+  OnResult(error);
+};
+
+const std::string& GetVersionCallback::getVersion()
+{
+  return version;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+GetLogCallback::GetLogCallback(std::shared_ptr<CallbackSynchronization> s) : KineticCallback(std::move(s))
+{ }
+
+GetLogCallback::~GetLogCallback()
+{ }
+
+void GetLogCallback::Success(unique_ptr<DriveLog> dlog)
+{
+  drive_log = std::move(dlog);
+  OnResult(KineticStatus(StatusCode::OK, ""));
+}
+
+void GetLogCallback::Failure(KineticStatus error)
+{
+  OnResult(error);
+}
+
+shared_ptr<DriveLog>& GetLogCallback::getLog()
+{
+  return drive_log;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+PutCallback::PutCallback(std::shared_ptr<CallbackSynchronization> s) : KineticCallback(std::move(s))
+{ }
+
+PutCallback::~PutCallback()
+{ }
+
+void PutCallback::Success()
+{
+  OnResult(KineticStatus(StatusCode::OK, ""));
+}
+
+void PutCallback::Failure(KineticStatus error)
+{
+  OnResult(error);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+BasicCallback::BasicCallback(std::shared_ptr<CallbackSynchronization> s) : KineticCallback(std::move(s))
+{ }
+
+BasicCallback::~BasicCallback()
+{ }
+
+void BasicCallback::Success()
+{
+  OnResult(KineticStatus(StatusCode::OK, ""));
+}
+
+void BasicCallback::Failure(KineticStatus error)
+{
+  OnResult(error);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+RangeCallback::RangeCallback(std::shared_ptr<CallbackSynchronization> s) : KineticCallback(std::move(s))
+{ }
+
+RangeCallback::~RangeCallback()
+{ }
+
+void RangeCallback::Success(unique_ptr<vector<string>> k)
+{
+  keys = std::move(k);
+  OnResult(KineticStatus(StatusCode::OK, ""));
+}
+
+void RangeCallback::Failure(KineticStatus error)
+{
+  OnResult(error);
+};
+
+unique_ptr<vector<string> >& RangeCallback::getKeys()
+{
+  return keys;
+}
--- throughput.cc
+++ throughput.cc
@@ -5,6 +5,7 @@
 #include <iomanip>
 #include <algorithm>
 #include "kinetic/kinetic.h"
+#include "KineticCallbacks.hh"
 
 using kinetic::KineticConnectionFactory;
 using kinetic::KineticRecord;
@@ -17,24 +18,25 @@ using namespace std::chrono;
 enum class OperationType {
   PUT, GET, DEL, LOG
 };
-enum class conselect{
-  HASH, FIXED
-};
 
-struct configuration{
-  int num_threads;
-  int num_keys;
-  int starting_key;
-  int value_size;
-  int report_keys;
-  bool flush_on_report;
-  kinetic::PersistMode persist;
-  conselect select;
-  vector<string> hosts;
-  vector<OperationType> ops;
+struct configuration {
+  std::string host;
   int security_id;
   string security_key;
+
+  int start_key;
+  int num_keys;
+  int report_keys;
   int random_sequence_size;
+
+  int put_value_size;
+  int put_key_size;
+
+  kinetic::PersistMode persist;
+  bool flush_on_report;
+  int queue_depth;
+
+  vector<OperationType> ops;
 };
 
 string to_str(OperationType type){
@@ -50,16 +52,18 @@ string to_str(OperationType type){
 void parse(int argc, char** argv, configuration &config)
 {
   for(int i = 1; i+1 < argc; i++){
-    if(strcmp("-threads", argv[i]) == 0)
-      config.num_threads = std::stoi(argv[i+1]);
     if(strcmp("-keys", argv[i]) == 0)
       config.num_keys = std::stoi(argv[i+1]);
     if(strcmp("-starting_key", argv[i]) == 0)
-      config.starting_key = std::stoi(argv[i+1]);
-    if(strcmp("-size", argv[i]) == 0)
-      config.value_size = std::stoi(argv[i+1]);
+      config.start_key = std::stoi(argv[i+1]);
+    if(strcmp("-key_size", argv[i]) == 0)
+      config.put_key_size = std::stoi(argv[i+1]);
+    if(strcmp("-value_size", argv[i]) == 0)
+      config.put_value_size = std::stoi(argv[i+1]);
+    if(strcmp("-queue_depth", argv[i]) == 0)
+      config.queue_depth = std::stoi(argv[i+1]);
     if(strcmp("-host", argv[i]) == 0)
-      config.hosts.push_back(argv[i+1]);
+      config.host = argv[i+1];
     if(strcmp("-report", argv[i]) == 0)
       config.report_keys = std::stoi(argv[i+1]);
     if(strcmp("-id", argv[i]) == 0)
@@ -76,10 +80,6 @@ void parse(int argc, char** argv, configuration &config)
       if(strcmp(argv[i+1],"enabled")==0)
         config.flush_on_report = true;
     }
-    if(strcmp("-select", argv[i]) == 0){
-      if(strcmp(argv[i+1],"fixed")==0)
-        config.select = conselect::FIXED;
-    }
     if(strcmp("-op", argv[i]) == 0){
       if(strcmp(argv[i+1],"put")==0)
         config.ops.push_back(OperationType::PUT);
@@ -92,7 +92,7 @@ void parse(int argc, char** argv, configuration &config)
     }
   }
 
-  if(!config.report_keys)
+  if(!config.report_keys || config.report_keys > config.num_keys)
     config.report_keys = config.num_keys;
   if(!config.random_sequence_size)
     config.random_sequence_size = config.num_keys;
@@ -100,114 +100,89 @@ void parse(int argc, char** argv, configuration &config)
   printf("---------------------------------------------------------- \n");
   printf("                cpp client throughput test               \n");
   printf("---------------------------------------------------------- \n");
-  printf("  Selected hosts: ");
-  for(size_t i = 0; i < config.hosts.size(); i++)
-      printf( " -host %s",config.hosts[i].c_str());
   printf("\n  Selected operation sequence {from put, get, del, log}: ");
-  for(size_t i = 0; i < config.ops.size(); i++)
+  for(size_t i = 0; i < config.ops.size(); i++) {
     printf(" -op %s", to_str(config.ops[i]).c_str());
-  printf( "\n  Secret key used for connection(s): -key %s \n"
-            "  Identity used for connection(s): -id %d \n"
-            "  Number of threads: -threads %d \n"
+  }
+  printf( "\n  Selected hosts: -host %s\n"
+            "  Queue Depth for requests: -queue_depth %d \n"
+            "  Secret key used for connection: -key %s \n"
+            "  Identity used for connection: -id %d \n"
             "  Number of keys: -keys %d \n"
             "  Starting key number: -starting_key %d\n"
-            "  Size of key value in KB: -size %d \n"
+            "  Size of key in Byte: -key_size %d \n"
+            "  Size of value in KB: -value_size %d \n"
             "  Report average performance ever n keys: -report %d \n"
             "  Flush connection before reporting {enabled,disabled}: -flush %s \n"
             "  Put / Delete persistence {write_back,write_through}: -persist %s \n"
-            "  Selection strategy for connections {hash,fixed}: -select %s \n"
             "  Randomize key every n sequential operations: -ran_seq %d \n",
-          config.security_key.c_str(), config.security_id,
-          config.num_threads, config.num_keys, config.starting_key, config.value_size, config.report_keys,
+          config.host.c_str(), config.queue_depth, config.security_key.c_str(), config.security_id,
+          config.num_keys, config.start_key, config.put_key_size, config.put_value_size, config.report_keys,
           config.flush_on_report ? "enabled" : "disabled",
           config.persist ==  kinetic::PersistMode::WRITE_BACK ? "write_back" : "write_through",
-          config.select == conselect::HASH ? "hash" : "fixed",
           config.random_sequence_size);
-          config.value_size*=1024;
   printf("---------------------------------------------------------- \n");
+  config.put_value_size*=1024;
 }
-
-
-
-void connect(const configuration &config, vector<shared_ptr<kinetic::BlockingKineticConnectionInterface>> &cons)
-{
-  kinetic::KineticConnectionFactory factory = kinetic::NewKineticConnectionFactory();
-
-  for(size_t i=0; i<config.hosts.size(); i++){
-    kinetic::ConnectionOptions options;
-    options.host = config.hosts[i];
-    options.port = 8123;
-    options.user_id = config.security_id;
-    options.hmac_key = config.security_key;
-    options.use_ssl = false;
-
-    shared_ptr<kinetic::ThreadsafeBlockingKineticConnection> con;
-    factory.NewThreadsafeBlockingConnection(options, con, 300);
-    if(con)
-      cons.push_back(con);
-  }
-}
-
-void test(int thread_id,
-          int operationFrequency,
-          OperationType operationType,
+void test(
+          int numOperations, OperationType operationType,
           std::list<uint32_t>::iterator& operationKeys,
           const configuration& config,
-          vector<shared_ptr<kinetic::BlockingKineticConnectionInterface>>& cons)
+          std::unique_ptr <kinetic::ThreadsafeNonblockingKineticConnection>& con)
 {
-  string value(config.value_size, 'X');
-  int connectionID = thread_id % cons.size();
+  string value(config.put_value_size, 'X');
+  auto sync = std::make_shared<kio::CallbackSynchronization>();
 
-  for (int i = 0; i < operationFrequency; i++) {
+  for (int i = 0; i < numOperations; i++) {
+
     std::ostringstream ss;
-    ss << std::setw(20) << std::setfill('0') << *operationKeys++;
+    ss << std::setw(config.put_key_size) << std::setfill('0') << *operationKeys++;
     auto key = ss.str();
 
-    //printf("thread %d -> %s\n",thread_id,key.c_str());
 
-    if(config.select == conselect::HASH) {
-      connectionID = std::hash<string>()(key) % cons.size();
-    }
 
     kinetic::KineticStatus status = kinetic::KineticStatus(kinetic::StatusCode::REMOTE_OTHER_ERROR, "");
     switch (operationType) {
       case OperationType::PUT: {
-        KineticRecord record(value, std::to_string((long long int) i), std::to_string((long long int) i),
+        auto cb = std::make_shared<kio::PutCallback>(sync);
+        auto record = std::make_shared<KineticRecord>(value,
+                             std::to_string((long long int) i), std::to_string((long long int) i),
                              com::seagate::kinetic::client::proto::Command_Algorithm_SHA1);
-        status = cons[connectionID]->Put(key, "", WriteMode::IGNORE_VERSION, record, config.persist);
+        con->Put(key, "", WriteMode::IGNORE_VERSION, record, cb, config.persist);
       }
         break;
       case OperationType::GET: {
-        std::unique_ptr<KineticRecord> record;
-        status = cons[connectionID]->Get(key, record);
+        auto cb = std::make_shared<kio::GetCallback>(sync);
+        con->Get(key, cb);
       }
         break;
       case OperationType::DEL: {
-        status = cons[connectionID]->Delete(key, "", WriteMode::IGNORE_VERSION, config.persist);
+        auto cb = std::make_shared<kio::BasicCallback>(sync);
+        con->Delete(key, "", WriteMode::IGNORE_VERSION, cb, config.persist);
       }
         break;
       case OperationType::LOG: {
-        std::unique_ptr<kinetic::DriveLog> log;
-        status = cons[connectionID]->GetLog({kinetic::Command_GetLog_Type::Command_GetLog_Type_UTILIZATIONS}, log);
+        auto cb = std::make_shared<kio::GetLogCallback>(sync);
+        con->GetLog({kinetic::Command_GetLog_Type::Command_GetLog_Type_UTILIZATIONS}, cb);
       }
         break;
     }
-    if (!status.ok()) {
-      printf("ERROR DURING %s OPERATION: %s \n", to_str(operationType).c_str(), status.message().c_str());
-    }
+    sync->run_until(con, config.queue_depth);
   }
 
   if (config.flush_on_report) {
-    cons[connectionID]->Flush();
+    auto cb = std::make_shared<kio::BasicCallback>(sync);
+    con->Flush(cb);
   }
+  sync->run_until(con, 1);
 }
 
 std::list<uint32_t> constructAccessList(const struct configuration& config) {
 
   std::vector<std::list<uint32_t>> sequence_vector(config.num_keys / config.random_sequence_size);
   for (size_t sequence = 0; sequence < sequence_vector.size(); sequence++) {
-    for (size_t number = sequence * config.random_sequence_size;
-         number < (sequence + 1) * config.random_sequence_size; number++) {
+    for (size_t number = sequence * config.random_sequence_size + config.start_key;
+         number < (sequence + 1) * config.random_sequence_size + config.start_key; number++) {
       sequence_vector[sequence].push_back(number);
     }
   }
@@ -222,13 +197,22 @@ std::list<uint32_t> constructAccessList(const struct configuration& config) {
 
 int main(int argc, char** argv)
 {
-  struct configuration config = {1,100,0,0,0,false,kinetic::PersistMode::WRITE_BACK,conselect::FIXED,{},{},1,"asdfasdf",0};
+  configuration config = {"localhost",1,"asdfasdf",0,100,0,0,1024,16, kinetic::PersistMode::WRITE_BACK, false, 1, {}};
   parse(argc, argv, config);
 
-  vector<shared_ptr<kinetic::BlockingKineticConnectionInterface>> cons;
-  connect(config,cons);
-  if(cons.empty() || config.ops.empty()){
-    printf("\n Invalid configuration // Specify -host and -op \n");
+  kinetic::KineticConnectionFactory factory = kinetic::NewKineticConnectionFactory();
+  kinetic::ConnectionOptions options;
+  options.host = config.host;
+  options.port = 8123;
+  options.user_id = config.security_id;
+  options.hmac_key = config.security_key;
+  options.use_ssl = false;
+
+  std::unique_ptr <kinetic::ThreadsafeNonblockingKineticConnection> con;
+  factory.NewThreadsafeNonblockingConnection(options, con);
+
+  if(!con){
+    printf("\n No connection. \n");
     exit(0);
   }
 
@@ -238,36 +222,27 @@ int main(int argc, char** argv)
 
     auto op = config.ops[o];
     auto num_runs = config.num_keys / config.report_keys;
-    auto ops_per_thread = config.num_keys / (config.num_threads * num_runs);
+    auto ops_per_run = config.num_keys / num_runs;
 
     std::vector<std::list<uint32_t>::iterator> thread_iterators;
     std::list<uint32_t>::iterator cursor = access_list.begin();
-    for(int t = 0; t < config.num_threads; t++) {
-      thread_iterators.push_back(cursor);
-      std::advance(cursor, config.num_keys/config.num_threads);
-    }
+
+    thread_iterators.push_back(cursor);
+    std::advance(cursor, config.num_keys);
 
     for (int r = 0; r < num_runs; r++) {
-      vector<std::thread> threads;
+
       auto run_start = system_clock::now();
-
-      for (int i = 0; i < config.num_threads; i++) {
-        threads.push_back(std::thread(std::bind(
-            test, i, ops_per_thread, op, std::ref(thread_iterators[i]), std::cref(config), std::ref(cons))));
-      }
-      for (size_t t = 0; t < threads.size(); t++) {
-        threads[t].join();
-      }
-
+      test(ops_per_run, op, cursor, config, con);
       auto run_end = system_clock::now();
       int duration = (int) duration_cast<milliseconds>(run_end - run_start).count();
 
       printf("\n%s of %d keys done in %d milliseconds "
-                 "\n\t -->  %f MB/second"
-                 "\n\t -->  %f keys/second"
+                 "\n\t Values -->  %f MB/second"
+                 "\n\t Operations -->  %f keys/second"
                  "\n",
              to_str(op).c_str(), config.report_keys, duration,
-             (config.report_keys * ((float) config.value_size / (1024 * 1024))) / (duration / 1000.0),
+             (config.report_keys * ((float) config.put_value_size / (1024 * 1024))) / (duration / 1000.0),
              config.report_keys / (duration / 1000.0)
       );
       fflush(stdout);
